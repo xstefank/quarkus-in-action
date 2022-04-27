dc80f5a3904c518acc5b449b89ac7183521dafbb
Georgios Andrianakis <geoand@gmail.com>
2022-04-27 10:53:17 +0300

    Remove GraphQL from reservation service

--- reservation/src/main/java/org/acme/reservation/inventory/InventoryClient.java
+++
@@ -1,7 +0,0 @@
-package org.acme.reservation.inventory;
-
-import io.smallrye.graphql.client.typesafe.api.GraphQLClientApi;
-
-@GraphQLClientApi(configKey = "inventory")
-public interface InventoryClient {
-}
