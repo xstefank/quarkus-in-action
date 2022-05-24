1017069d10fbb4c3a61de448056665afd5668334
Jan Martiška <wraychus@gmail.com>
2022-05-24 09:58:46 +0200

    Rebase reservation-graphql on top of latest changes in reservation-rest

--- reservation-graphql/src/main/java/org/acme/reservation/rental/RentalClient.java
+++ reservation-graphql/src/main/java/org/acme/reservation/rental/RentalClient.java
@@ -4,7 +4,7 @@ import javax.ws.rs.POST;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 import org.jboss.resteasy.reactive.RestPath;
 
-@RegisterRestClient(baseUri = "http:localhost:8084")
+@RegisterRestClient(baseUri = "http://localhost:8082")
 public interface RentalClient {
 
     @POST
