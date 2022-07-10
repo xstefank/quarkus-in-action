27c43408a0efc84b9c5181f358fd6c4ade65446c
xstefank <xstefank122@gmail.com>
2022-07-10 17:48:57 +0200

    Add inventory to CH07

---
+++ CH07/inventory/src/main/java/org/acme/inventory/model/Car.java
@@ -0,0 +1,10 @@
+package org.acme.inventory.model;
+
+public class Car {
+
+    public Long id;
+    public String licensePlateNumber;
+    public String manufacturer;
+    public String model;
+
+}
---
+++ CH07/inventory/src/main/java/org/acme/inventory/service/InventoryService.java
@@ -0,0 +1,43 @@
+package org.acme.inventory.service;
+
+import org.acme.inventory.database.CarInventory;
+import org.acme.inventory.model.Car;
+import org.eclipse.microprofile.graphql.GraphQLApi;
+import org.eclipse.microprofile.graphql.Mutation;
+import org.eclipse.microprofile.graphql.Query;
+
+import javax.inject.Inject;
+import java.util.List;
+import java.util.Optional;
+
+@GraphQLApi
+public class InventoryService {
+
+    @Inject
+    CarInventory inventory;
+
+    @Query
+    public List<Car> cars() {
+        return inventory.getCars();
+    }
+
+    @Mutation
+    public Car register(Car car) {
+        inventory.getCars().add(car);
+        return car;
+    }
+
+    @Mutation
+    public boolean remove(String licensePlateNumber) {
+        List<Car> cars = inventory.getCars();
+        Optional<Car> toBeRemoved = cars.stream()
+            .filter(car -> car.licensePlateNumber.equals(licensePlateNumber))
+            .findAny();
+        if(toBeRemoved.isPresent()) {
+            return cars.remove(toBeRemoved.get());
+        } else {
+            return false;
+        }
+    }
+
+}
