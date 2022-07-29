4555ead95fee9fda22a753235e096cba48c1474a
xstefank <xstefank122@gmail.com>
2022-07-29 14:10:20 +0200

    Clear resources for recoding with Quarkus version 2.11.1.Final

--- CH07/inventory/src/main/resources/import.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO Car (id, licensePlateNumber, manufacturer, model) VALUES (NEXT VALUE FOR hibernate_sequence, 'ABC123', 'Mazda', '6');
-INSERT INTO Car (id, licensePlateNumber, manufacturer, model) VALUES (NEXT VALUE FOR hibernate_sequence, 'XYZ987', 'Ford', 'Mustang');
--- CH07/reservation/src/main/java/org/acme/reservation/rental/Rental.java
+++
@@ -1,4 +0,0 @@
-package org.acme.reservation.rental;
-
-public class Rental {
-}
