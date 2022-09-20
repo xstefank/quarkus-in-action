daed075d56ce2ca7e74a1986b4206cf4260c84e0
xstefank <xstefank122@gmail.com>
2022-09-20 13:45:25 +0200

    Update gRPC in chapter-04 to use CarResponse return type

--- chapter-04/inventory-service/src/main/java/org/acme/inventory/grpc/InventoryServiceImpl.java
+++ chapter-04/inventory-service/src/main/java/org/acme/inventory/grpc/InventoryServiceImpl.java
@@ -1,21 +1,17 @@
 package org.acme.inventory.grpc;
 
-import java.util.Optional;
-
-import javax.inject.Inject;
-
-import com.google.protobuf.Empty;
-
-import io.smallrye.mutiny.Multi;
+import io.quarkus.grpc.GrpcService;
+import io.smallrye.mutiny.Uni;
 import org.acme.inventory.database.CarInventory;
 import org.acme.inventory.model.Car;
+import org.acme.inventory.model.CarResponse;
 import org.acme.inventory.model.InsertCarRequest;
 import org.acme.inventory.model.InventoryService;
 import org.acme.inventory.model.RemoveCarRequest;
 import org.jboss.logging.Logger;
 
-import io.quarkus.grpc.GrpcService;
-import io.smallrye.mutiny.Uni;
+import javax.inject.Inject;
+import java.util.Optional;
 
 @GrpcService
 public class InventoryServiceImpl implements InventoryService {
@@ -26,22 +22,38 @@ public class InventoryServiceImpl implements InventoryService {
     CarInventory inventory;
 
     @Override
-    public Uni<Empty> add(Multi<InsertCarRequest> requests) {
-        return requests.onItem().invoke(request -> {
-            Car car = new Car();
-            car.licensePlateNumber = request.getLicensePlateNumber();
-            car.manufacturer = request.getManufacturer();
-            car.model = request.getModel();
-            LOGGER.error("Persisting " + car);
-            inventory.getCars().add(car);
-        }).collect().last().map(l -> Empty.newBuilder().build());
+    public Uni<CarResponse> add(InsertCarRequest request) {
+        Car car = new Car();
+        car.licensePlateNumber = request.getLicensePlateNumber();
+        car.manufacturer = request.getManufacturer();
+        car.model = request.getModel();
+        LOGGER.info("Persisting " + car);
+        inventory.getCars().add(car);
+
+        return Uni.createFrom().item(CarResponse.newBuilder()
+            .setLicensePlateNumber(car.licensePlateNumber)
+            .setManufacturer(car.manufacturer)
+            .setModel(car.model)
+            .build());
     }
 
     @Override
-    public Uni<Empty> remove(RemoveCarRequest request) {
+    public Uni<CarResponse> remove(RemoveCarRequest request) {
         Optional<Car> optionalCar = inventory.getCars().stream()
-            .filter(car -> request.getLicensePlateNumber().equals(car.licensePlateNumber)).findFirst();
-        optionalCar.ifPresent(car -> inventory.getCars().remove(car));
-        return Uni.createFrom().item(Empty.newBuilder().build());
+            .filter(car -> request.getLicensePlateNumber()
+                .equals(car.licensePlateNumber))
+            .findFirst();
+
+        if (optionalCar.isPresent()) {
+            Car removedCar = optionalCar.get();
+            inventory.getCars().remove(removedCar);
+            return Uni.createFrom().item(CarResponse.newBuilder()
+                .setLicensePlateNumber(removedCar.licensePlateNumber)
+                .setManufacturer(removedCar.manufacturer)
+                .setModel(removedCar.model)
+                .build());
+        }
+
+        return Uni.createFrom().nullItem();
     }
 }
