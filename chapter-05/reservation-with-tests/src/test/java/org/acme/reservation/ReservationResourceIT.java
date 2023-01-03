1410987f747ae757ce09633bb7b3074c7987bf1d
xstefank <xstefank122@gmail.com>
2023-01-03 14:40:09 +0100

    Clear resources for recoding for 1P

--- chapter-05/reservation-with-tests/src/test/java/org/acme/reservation/ReservationResourceIT.java
+++
@@ -1,8 +0,0 @@
-package org.acme.reservation;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class ReservationResourceIT extends ReservationResourceTest {
-
-}
