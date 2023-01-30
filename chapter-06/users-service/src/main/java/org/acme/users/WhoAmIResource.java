d5ed31094f4a1767ef47b4095d0852402646bba8
xstefank <xstefank122@gmail.com>
2023-01-30 10:04:27 +0100

    Section 6.1.1 - Add qute whoami template to users-service

---
+++ chapter-06/users-service/src/main/java/org/acme/users/WhoAmIResource.java
@@ -0,0 +1,29 @@
+package org.acme.users;
+
+import io.quarkus.qute.Template;
+import io.quarkus.qute.TemplateInstance;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.SecurityContext;
+
+@Path("/whoami")
+public class WhoAmIResource {
+
+    @Inject
+    Template whoami;
+
+    @Inject
+    SecurityContext securityContext;
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public TemplateInstance get() {
+        String userId = securityContext.getUserPrincipal() != null
+            ? securityContext.getUserPrincipal().getName() : null;
+        return whoami.data("name", userId);
+    }
+}
