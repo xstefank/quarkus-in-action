024f8bb908013de341d46042c05f66989b33c0c0
xstefank <xstefank122@gmail.com>
2023-02-02 12:58:14 +0100

    Section 6.2.3 - Implement UI in users-service

---
+++ chapter-06/users-service/src/main/java/org/acme/users/ReservationsResource.java
@@ -0,0 +1,103 @@
+package org.acme.users;
+
+import io.quarkus.qute.CheckedTemplate;
+import io.quarkus.qute.TemplateInstance;
+import io.smallrye.common.annotation.Blocking;
+import org.acme.users.model.Car;
+import org.acme.users.model.Reservation;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.jboss.resteasy.reactive.RestForm;
+import org.jboss.resteasy.reactive.RestQuery;
+import org.jboss.resteasy.reactive.RestResponse;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.SecurityContext;
+import java.time.LocalDate;
+import java.util.Collection;
+
+@Path("/")
+@Blocking
+public class ReservationsResource {
+
+    @CheckedTemplate
+    public static class Templates {
+        public static native TemplateInstance index(
+            LocalDate startDate,
+            LocalDate endDate,
+            String name);
+
+        public static native TemplateInstance listofreservations(
+            Collection<Reservation> reservations);
+
+        public static native TemplateInstance availablecars(
+            Collection<Car> cars,
+            LocalDate startDate,
+            LocalDate endDate);
+    }
+
+    @Inject
+    SecurityContext securityContext;
+
+    @RestClient
+    ReservationsClient client;
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public TemplateInstance index(@RestQuery LocalDate startDate,
+                                  @RestQuery LocalDate endDate) {
+        if (startDate == null) {
+            startDate = LocalDate.now().plusDays(1L);
+        }
+        if (endDate == null) {
+            endDate = LocalDate.now().plusDays(7);
+        }
+        return Templates.index(startDate, endDate,
+            securityContext.getUserPrincipal().getName());
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    @Path("/get")
+    public TemplateInstance getReservations() {
+        Collection<Reservation> reservationCollection
+            = client.allReservations();
+        return Templates.listofreservations(reservationCollection);
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    @Path("/available")
+    public TemplateInstance getAvailableCars(
+        @RestQuery LocalDate startDate,
+        @RestQuery LocalDate endDate) {
+        Collection<Car> availableCars
+            = client.availability(startDate, endDate);
+        return Templates.availablecars(
+            availableCars, startDate, endDate);
+    }
+
+    @POST
+    @Produces(MediaType.TEXT_HTML)
+    @Path("/reserve")
+    public RestResponse<TemplateInstance> create(
+        @RestForm LocalDate startDate,
+        @RestForm LocalDate endDate,
+        @RestForm Long carId) {
+        Reservation reservation = new Reservation();
+        reservation.startDay = startDate;
+        reservation.endDay = endDate;
+        reservation.carId = carId;
+        client.make(reservation);
+        return RestResponse.ResponseBuilder
+            .ok(getReservations())
+            .header("HX-Trigger-After-Swap",
+            "update-available-cars-list")
+        .build();
+    }
+
+}
