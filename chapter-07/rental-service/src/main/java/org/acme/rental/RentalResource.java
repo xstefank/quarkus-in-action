3d4b378163a05ceed9b17d016d1c2a0955c9af8b
xstefank <xstefank122@gmail.com>
2023-03-13 17:56:43 +0100

    Section 7.6.2 - Utilize Panache with MongoDB in the Rental service

--- chapter-07/rental-service/src/main/java/org/acme/rental/RentalResource.java
+++ chapter-07/rental-service/src/main/java/org/acme/rental/RentalResource.java
@@ -4,9 +4,14 @@ import io.quarkus.logging.Log;
 import org.acme.rental.entity.Rental;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicLong;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -24,7 +29,45 @@ public class RentalResource {
                         Long reservationId) {
         Log.infof("Starting rental for %s with reservation %s",
         userId, reservationId);
-        return new Rental(id.incrementAndGet(), userId, reservationId,
-            LocalDate.now());
+
+        Rental rental = new Rental();
+        rental.userId = userId;
+        rental.reservationId = reservationId;
+        rental.startDate = LocalDate.now();
+        rental.active = true;
+
+        rental.persist();
+
+        return rental;
+    }
+
+    @PUT
+    @Path("/end/{userId}/{reservationId}")
+    public Rental end(String userId, Long reservationId) {
+        Log.infof("Ending rental for %s with reservation %s",
+            userId, reservationId);
+        Optional<Rental> optionalRental = Rental
+            .findByUserAndReservationIdsOptional(userId, reservationId);
+
+        if (optionalRental.isPresent()) {
+            Rental rental = optionalRental.get();
+            rental.endDate = LocalDate.now();
+            rental.active = false;
+            rental.update();
+            return rental;
+        } else {
+            throw new NotFoundException("Rental not found");
+        }
+    }
+
+    @GET
+    public List<Rental> list() {
+        return Rental.listAll();
+    }
+
+    @GET
+    @Path("/active")
+    public List<Rental> listActive() {
+        return Rental.listActive();
     }
 }
--- chapter-07/rental-service/src/main/java/org/acme/rental/entity/Rental.java
+++ chapter-07/rental-service/src/main/java/org/acme/rental/entity/Rental.java
@@ -3,12 +3,26 @@ package org.acme.rental.entity;
 import io.quarkus.mongodb.panache.PanacheMongoEntity;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 public class Rental extends PanacheMongoEntity {
 
-    public Long userId;
+    public String userId;
     public Long reservationId;
     public LocalDate startDate;
     public LocalDate endDate;
     public boolean active;
+
+    public static Optional<Rental> findByUserAndReservationIdsOptional(
+        String userId, Long reservationId) {
+        return find("userId = ?1 and reservationId = ?2",
+            userId, reservationId)
+            .firstResultOptional();
+    }
+
+    public static List<Rental> listActive() {
+        return list("active", true);
+    }
+
 }
