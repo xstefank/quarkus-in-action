46b3fc9497ff482ecb45482e36c28063bfa5ae7e
xstefank <xstefank122@gmail.com>
2023-06-25 12:52:16 +0200

    Chapter 09 - add rental-service

---
+++ chapter-09/rental-service/src/main/java/org/acme/rental/RentalResource.java
@@ -0,0 +1,71 @@
+package org.acme.rental;
+
+import io.quarkus.logging.Log;
+import org.acme.rental.entity.Rental;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/rental")
+public class RentalResource {
+
+    @Path("/start/{userId}/{reservationId}")
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Rental start(String userId,
+                        Long reservationId) {
+        Log.infof("Starting rental for %s with reservation %s",
+        userId, reservationId);
+
+        Rental rental = new Rental();
+        rental.userId = userId;
+        rental.reservationId = reservationId;
+        rental.startDate = LocalDate.now();
+        rental.active = true;
+
+        rental.persist();
+
+        return rental;
+    }
+
+    @PUT
+    @Path("/end/{userId}/{reservationId}")
+    public Rental end(String userId, Long reservationId) {
+        Log.infof("Ending rental for %s with reservation %s",
+            userId, reservationId);
+        Optional<Rental> optionalRental = Rental
+            .findByUserAndReservationIdsOptional(userId, reservationId);
+
+        if (optionalRental.isPresent()) {
+            Rental rental = optionalRental.get();
+            rental.endDate = LocalDate.now();
+            rental.active = false;
+            rental.update();
+            return rental;
+        } else {
+            throw new NotFoundException("Rental not found");
+        }
+    }
+
+    @GET
+    public List<Rental> list() {
+        return Rental.listAll();
+    }
+
+    @GET
+    @Path("/active")
+    public List<Rental> listActive() {
+        return Rental.listActive();
+    }
+}
