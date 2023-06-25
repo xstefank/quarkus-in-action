f7c364a55e8d7c5f383aeabdf7a1d1d7f014b457
xstefank <xstefank122@gmail.com>
2023-06-25 10:46:35 +0200

    Section 9.2 - Reactive messaging in Reservation

--- chapter-09/reservation-service/src/main/java/org/acme/reservation/rest/ReservationResource.java
+++ chapter-09/reservation-service/src/main/java/org/acme/reservation/rest/ReservationResource.java
@@ -1,12 +1,21 @@
 package org.acme.reservation.rest;
 
-import java.time.LocalDate;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
+import io.quarkus.hibernate.reactive.panache.common.runtime.ReactiveTransactional;
+import io.quarkus.logging.Log;
+import io.smallrye.graphql.client.GraphQLClient;
+import io.smallrye.mutiny.Uni;
+import io.smallrye.reactive.messaging.MutinyEmitter;
+import org.acme.reservation.billing.Invoice;
+import org.acme.reservation.entity.Reservation;
+import org.acme.reservation.inventory.Car;
+import org.acme.reservation.inventory.GraphQLInventoryClient;
+import org.acme.reservation.inventory.InventoryClient;
+import org.acme.reservation.rental.RentalClient;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.jboss.resteasy.reactive.RestQuery;
+
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -15,22 +24,24 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.SecurityContext;
-
-import io.quarkus.hibernate.reactive.panache.common.runtime.ReactiveTransactional;
-import io.quarkus.logging.Log;
-import io.smallrye.graphql.client.GraphQLClient;
-import io.smallrye.mutiny.Uni;
-import org.acme.reservation.inventory.Car;
-import org.acme.reservation.inventory.GraphQLInventoryClient;
-import org.acme.reservation.inventory.InventoryClient;
-import org.acme.reservation.rental.RentalClient;
-import org.acme.reservation.entity.Reservation;
-import org.eclipse.microprofile.rest.client.inject.RestClient;
-import org.jboss.resteasy.reactive.RestQuery;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletionStage;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Path("reservation")
 @Produces(MediaType.APPLICATION_JSON)
 public class ReservationResource {
+
+    private static final double STANDARD_RATE_PER_DAY = 19.99;
+
+    @Inject
+    @Channel("invoices")
+    MutinyEmitter<Invoice> invoiceEmitter;
 
     private final InventoryClient inventoryClient;
     private final RentalClient rentalClient;
@@ -39,9 +50,9 @@ public class ReservationResource {
     SecurityContext context;
 
     public ReservationResource(
-                               @GraphQLClient("inventory")
-                               GraphQLInventoryClient inventoryClient,
-                               @RestClient RentalClient rentalClient) {
+        @GraphQLClient("inventory")
+        GraphQLInventoryClient inventoryClient,
+        @RestClient RentalClient rentalClient) {
         this.inventoryClient = inventoryClient;
         this.rentalClient = rentalClient;
     }
@@ -52,19 +63,29 @@ public class ReservationResource {
     public Uni<Reservation> make(Reservation reservation) {
         reservation.userId = context.getUserPrincipal() != null ?
             context.getUserPrincipal().getName() : "anonymous";
+
         return reservation.<Reservation>persist().onItem()
             .call(persistedReservation -> {
                 Log.info("Successfully reserved reservation " + persistedReservation);
+
+                Uni<Void> invoiceUni = invoiceEmitter.send(
+                    new Invoice(reservation, computePrice(reservation)));
+
                 if (persistedReservation.startDay.equals(LocalDate.now())) {
-                    return rentalClient.start(persistedReservation.userId,
+                    return invoiceUni.chain(() ->
+                        rentalClient.start(persistedReservation.userId,
                             persistedReservation.id)
                         .onItem().invoke(rental ->
                             Log.info("Successfully started rental " + rental))
-                        .replaceWith(persistedReservation);
-
+                        .replaceWith(persistedReservation));
                 }
                 return Uni.createFrom().item(persistedReservation);
             });
+    }
+
+    private double computePrice(Reservation reservation) {
+        return Duration.between(reservation.startDay.atStartOfDay(),
+            reservation.endDay.atStartOfDay()).toDays() * STANDARD_RATE_PER_DAY;
     }
 
     @GET
@@ -81,7 +102,7 @@ public class ReservationResource {
     @GET
     @Path("availability")
     public Uni<Collection<Car>> availability(@RestQuery LocalDate startDate,
-                                        @RestQuery LocalDate endDate) {
+                                             @RestQuery LocalDate endDate) {
         Uni<Map<Long, Car>> carsUni = inventoryClient.allCars()
             .map(cars -> cars.stream().collect(Collectors
                 .toMap(car -> car.id, Function.identity())));
