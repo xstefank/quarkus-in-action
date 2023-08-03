f58fd5122b5eed0a59312e5e73d60454590cdf03
xstefank <xstefank122@gmail.com>
2023-08-03 10:40:24 +0200

    Fix sequence name in CH07

--- chapter-07/inventory-service/src/main/resources/import.sql
+++ chapter-07/inventory-service/src/main/resources/import.sql
@@ -1,2 +1,2 @@
-INSERT INTO Car (id, licensePlateNumber, manufacturer, model) VALUES (NEXT VALUE FOR hibernate_sequence, 'ABC123', 'Mazda', '6');
-INSERT INTO Car (id, licensePlateNumber, manufacturer, model) VALUES (NEXT VALUE FOR hibernate_sequence, 'XYZ987', 'Ford', 'Mustang');
+INSERT INTO Car (id, licensePlateNumber, manufacturer, model) VALUES (NEXT VALUE FOR Car_SEQ, 'ABC123', 'Mazda', '6');
+INSERT INTO Car (id, licensePlateNumber, manufacturer, model) VALUES (NEXT VALUE FOR Car_SEQ, 'XYZ987', 'Ford', 'Mustang');
