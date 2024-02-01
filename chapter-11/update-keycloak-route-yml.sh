b6e37bbb768f9b2fc05eefc074e794f6439a9273
xstefank <xstefank122@gmail.com>
2024-02-01 10:42:30 +0100

    Remove update-keycloak-route-yml.sh since the target OpenShift might not be SandBox

--- chapter-11/update-keycloak-route-yml.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -euo pipefail
-
-# this will fail the script if the route is not found
-KEYCLOAK_ROUTE=$(oc get route keycloak -o jsonpath="{.spec.host}")
-
-sed -i "s#value: http://keycloak-.*/realms/car-rental#value: http://$KEYCLOAK_ROUTE/realms/car-rental#g" ./car-rental.yml
