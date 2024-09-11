a01187815e489c0baac16ee6313ad4785b8bce9e
xstefank <xstefank122@gmail.com>
2024-09-11 16:42:08 +0200

    Start - 3.4.1 Securing the Quarkus application using OpenID Connect and Dev Services

---
+++ chapter-03/3_4_1/quarkus-in-action/src/main/java/org/acme/GreetingResource.java
@@ -0,0 +1,20 @@
+package org.acme;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @ConfigProperty(name = "greeting")
+    String greeting;
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return greeting;
+    }
+}
