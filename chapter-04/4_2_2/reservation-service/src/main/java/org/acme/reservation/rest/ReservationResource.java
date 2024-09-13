717fcad9d03c81710bb3b13232792297c1404fb4
xstefank <xstefank122@gmail.com>
2024-09-13 10:44:51 +0200

    Start - 4.2.2 Making a reservation

---
+++ chapter-04/4_2_2/reservation-service/src/main/java/org/acme/reservation/rest/ReservationResource.java
@@ -0,0 +1,54 @@
+package org.acme.reservation.rest;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import org.acme.reservation.inventory.Car;
+import org.acme.reservation.inventory.InventoryClient;
+import org.acme.reservation.reservation.Reservation;
+import org.acme.reservation.reservation.ReservationsRepository;
+import org.jboss.resteasy.reactive.RestQuery;
+
+
+@Path("reservation")
+@Produces(MediaType.APPLICATION_JSON)
+public class ReservationResource {
+
+    private final ReservationsRepository reservationsRepository;
+    private final InventoryClient inventoryClient;
+
+    public ReservationResource(ReservationsRepository reservations,
+                               InventoryClient inventoryClient) {
+        this.reservationsRepository = reservations;
+        this.inventoryClient = inventoryClient;
+    }
+
+    @GET
+    @Path("availability")
+    public Collection<Car> availability(@RestQuery LocalDate startDate,
+                                        @RestQuery LocalDate endDate) {
+        // obtain all cars from inventory
+        List<Car> availableCars = inventoryClient.allCars();
+        // create a map from id to car
+        Map<Long, Car> carsById = new HashMap<>();
+        for (Car car : availableCars) {
+            carsById.put(car.id, car);
+        }
+
+        // get all current reservations
+        List<Reservation> reservations = reservationsRepository.findAll();
+        // for each reservation, remove the car from the map
+        for (Reservation reservation : reservations) {
+            if (reservation.isReserved(startDate, endDate)) {
+                carsById.remove(reservation.carId);
+            }
+        }
+        return carsById.values();
+    }
+}
