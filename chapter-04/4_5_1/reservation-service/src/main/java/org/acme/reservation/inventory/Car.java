00519239373e73176e7ade72c3c6781bf18882bb
xstefank <xstefank122@gmail.com>
2024-09-13 14:00:02 +0200

    Start - 4.5.1 Exposing the Inventory service over GraphQL

---
+++ chapter-04/4_5_1/reservation-service/src/main/java/org/acme/reservation/inventory/Car.java
@@ -0,0 +1,17 @@
+package org.acme.reservation.inventory;
+
+public class Car {
+
+    public Long id;
+    public String licensePlateNumber;
+    public String manufacturer;
+    public String model;
+
+    public Car(Long id, String licensePlateNumber,
+               String manufacturer, String model) {
+        this.id = id;
+        this.licensePlateNumber = licensePlateNumber;
+        this.manufacturer = manufacturer;
+        this.model = model;
+    }
+}
