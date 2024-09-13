65443535363a4a2d22356a73d326ad010b5ec341
xstefank <xstefank122@gmail.com>
2024-09-13 11:22:49 +0200

    4.3 Using the REST Client

--- chapter-04/4_3/reservation-service/src/main/java/org/acme/reservation/rest/ReservationResource.java
+++ chapter-04/4_3/reservation-service/src/main/java/org/acme/reservation/rest/ReservationResource.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import io.quarkus.logging.Log;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
@@ -14,8 +15,11 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import org.acme.reservation.inventory.Car;
 import org.acme.reservation.inventory.InventoryClient;
+import org.acme.reservation.rental.Rental;
+import org.acme.reservation.rental.RentalClient;
 import org.acme.reservation.reservation.Reservation;
 import org.acme.reservation.reservation.ReservationsRepository;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.jboss.resteasy.reactive.RestQuery;
 
 
@@ -25,11 +29,27 @@ public class ReservationResource {
 
     private final ReservationsRepository reservationsRepository;
     private final InventoryClient inventoryClient;
+    private final RentalClient rentalClient;
 
     public ReservationResource(ReservationsRepository reservations,
-                               InventoryClient inventoryClient) {
+                               InventoryClient inventoryClient,
+                               @RestClient RentalClient rentalClient) {
         this.reservationsRepository = reservations;
         this.inventoryClient = inventoryClient;
+        this.rentalClient = rentalClient;
+    }
+
+    @Consumes(MediaType.APPLICATION_JSON)
+    @POST
+    public Reservation make(Reservation reservation) {
+        Reservation result = reservationsRepository.save(reservation);
+        // this is just a dummy value for the time being
+        String userId = "x";
+        if (reservation.startDay.equals(LocalDate.now())) {
+            Rental rental = rentalClient.start(userId, result.id);
+            Log.info("Successfully started rental " + rental);
+        }
+        return result;
     }
 
     @GET
@@ -53,11 +73,5 @@ public class ReservationResource {
             }
         }
         return carsById.values();
-    }
-
-    @Consumes(MediaType.APPLICATION_JSON)
-    @POST
-    public Reservation make(Reservation reservation) {
-        return reservationsRepository.save(reservation);
     }
 }
