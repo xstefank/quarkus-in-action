b916160147ef9f53e49c956b1e0de10d15c98d95
xstefank <xstefank122@gmail.com>
2024-09-25 17:32:02 +0200

    7.6.2 Using the Panache MongoDB in Rental service

--- chapter-07/7_6_2/rental-service/src/main/java/org/acme/rental/RentalResource.java
+++ chapter-07/7_6_2/rental-service/src/main/java/org/acme/rental/RentalResource.java
@@ -1,28 +1,65 @@
 package org.acme.rental;
 
 import io.quarkus.logging.Log;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.NotFoundException;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import org.acme.rental.entity.Rental;
 
 import java.time.LocalDate;
-import java.util.concurrent.atomic.AtomicLong;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
-import org.acme.rental.entity.Rental;
+import java.util.List;
+import java.util.Optional;
 
 @Path("/rental")
 public class RentalResource {
 
-    private final AtomicLong id = new AtomicLong(0);
-
     @Path("/start/{userId}/{reservationId}")
     @POST
-    @Produces(MediaType.APPLICATION_JSON)
     public Rental start(String userId,
                         Long reservationId) {
         Log.infof("Starting rental for %s with reservation %s",
-        userId, reservationId);
-        return new Rental(id.incrementAndGet(), userId, reservationId,
-            LocalDate.now());
+            userId, reservationId);
+
+        Rental rental = new Rental();
+        rental.userId = userId;
+        rental.reservationId = reservationId;
+        rental.startDate = LocalDate.now();
+        rental.active = true;
+
+        rental.persist();
+
+        return rental;
+    }
+
+    @PUT
+    @Path("/end/{userId}/{reservationId}")
+    public Rental end(String userId, Long reservationId) {
+        Log.infof("Ending rental for %s with reservation %s",
+            userId, reservationId);
+        Optional<Rental> optionalRental = Rental
+            .findByUserAndReservationIdsOptional(userId, reservationId);
+
+        if (optionalRental.isPresent()) {
+            Rental rental = optionalRental.get();
+            rental.endDate = LocalDate.now();
+            rental.active = false;
+            rental.update();
+            return rental;
+        } else {
+            throw new NotFoundException("Rental not found");
+        }
+    }
+
+    @GET
+    public List<Rental> list() {
+        return Rental.listAll();
+    }
+
+    @GET
+    @Path("/active")
+    public List<Rental> listActive() {
+        return Rental.listActive();
     }
 }
