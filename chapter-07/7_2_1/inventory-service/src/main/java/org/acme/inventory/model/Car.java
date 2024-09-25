83e5c24e4bac05fc5a44f44d4b73db6dc06d9ff0
xstefank <xstefank122@gmail.com>
2024-09-25 10:29:45 +0200

    7.2.1 Utilizing repository pattern with MySQL

--- chapter-07/7_2_1/inventory-service/src/main/java/org/acme/inventory/model/Car.java
+++ chapter-07/7_2_1/inventory-service/src/main/java/org/acme/inventory/model/Car.java
@@ -1,10 +1,76 @@
 package org.acme.inventory.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Objects;
+
+@Entity
 public class Car {
 
-    public Long id;
-    public String licensePlateNumber;
-    public String manufacturer;
-    public String model;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String licensePlateNumber;
+    private String manufacturer;
+    private String model;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLicensePlateNumber() {
+        return licensePlateNumber;
+    }
+
+    public void setLicensePlateNumber(String licensePlateNumber) {
+        this.licensePlateNumber = licensePlateNumber;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+            "id=" + id +
+            ", licensePlateNumber='" + licensePlateNumber + '\'' +
+            ", manufacturer='" + manufacturer + '\'' +
+            ", model='" + model + '\'' +
+            '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Car car)) return false;
+        return Objects.equals(getId(), car.getId())
+            && Objects.equals(getLicensePlateNumber(), car.getLicensePlateNumber())
+            && Objects.equals(getManufacturer(), car.getManufacturer())
+            && Objects.equals(getModel(), car.getModel());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getLicensePlateNumber(), getManufacturer(), getModel());
+    }
 }
