372a573a65af41465d8fb793fa0088c540a18b1f
xstefank <xstefank122@gmail.com>
2024-09-25 17:32:02 +0200

    7.6.2 Using the Panache MongoDB in Rental service

--- chapter-07/7_6_2/rental-service/src/main/java/org/acme/rental/entity/Rental.java
+++ chapter-07/7_6_2/rental-service/src/main/java/org/acme/rental/entity/Rental.java
@@ -3,6 +3,8 @@ package org.acme.rental.entity;
 import io.quarkus.mongodb.panache.PanacheMongoEntity;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 public class Rental extends PanacheMongoEntity {
 
@@ -11,6 +13,17 @@ public class Rental extends PanacheMongoEntity {
     public LocalDate startDate;
     public LocalDate endDate;
     public boolean active;
+
+    public static Optional<Rental> findByUserAndReservationIdsOptional(
+        String userId, Long reservationId) {
+        return find("userId = ?1 and reservationId = ?2",
+            userId, reservationId)
+            .firstResultOptional();
+    }
+
+    public static List<Rental> listActive() {
+        return list("active", true);
+    }
 
     @Override
     public String toString() {
