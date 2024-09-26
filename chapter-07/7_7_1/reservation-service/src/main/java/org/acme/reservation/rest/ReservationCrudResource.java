0012e59ec2c826933054ce1fb30cbe2c7ee81b73
xstefank <xstefank122@gmail.com>
2024-09-26 08:24:09 +0200

    7.7.1 Using Hibernate Reactive with Panache

--- chapter-07/7_7_1/reservation-service/src/main/java/org/acme/reservation/rest/ReservationCrudResource.java
+++ chapter-07/7_7_1/reservation-service/src/main/java/org/acme/reservation/rest/ReservationCrudResource.java
@@ -1,6 +1,6 @@
 package org.acme.reservation.rest;
 
-import io.quarkus.hibernate.orm.rest.data.panache.PanacheEntityResource;
+import io.quarkus.hibernate.reactive.rest.data.panache.PanacheEntityResource;
 import io.quarkus.rest.data.panache.ResourceProperties;
 import org.acme.reservation.entity.Reservation;
 
