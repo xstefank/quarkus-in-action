d0105fa897fd43acea819f9959bef7e25efa1a97
xstefank <xstefank122@gmail.com>
2024-09-26 08:24:09 +0200

    7.7.1 Using Hibernate Reactive with Panache

--- chapter-07/7_7_1/reservation-service/src/main/java/org/acme/reservation/entity/Reservation.java
+++ chapter-07/7_7_1/reservation-service/src/main/java/org/acme/reservation/entity/Reservation.java
@@ -1,6 +1,6 @@
 package org.acme.reservation.entity;
 
-import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import io.quarkus.hibernate.reactive.panache.PanacheEntity;
 import jakarta.persistence.Entity;
 
 import java.time.LocalDate;
