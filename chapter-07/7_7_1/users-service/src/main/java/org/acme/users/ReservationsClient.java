6a582407a860773c497104807d125ae4f20be15b
xstefank <xstefank122@gmail.com>
2024-09-26 08:18:23 +0200

    Start - 7.7.1 Using Hibernate Reactive with Panache

---
+++ chapter-07/7_7_1/users-service/src/main/java/org/acme/users/ReservationsClient.java
@@ -0,0 +1,31 @@
+package org.acme.users;
+
+import io.quarkus.oidc.token.propagation.AccessToken;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import org.acme.users.model.Car;
+import org.acme.users.model.Reservation;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+import org.jboss.resteasy.reactive.RestQuery;
+
+import java.time.LocalDate;
+import java.util.Collection;
+
+@RegisterRestClient(baseUri = "http://localhost:8081")
+@AccessToken
+@Path("reservation")
+public interface ReservationsClient {
+
+    @GET
+    @Path("all")
+    Collection<Reservation> allReservations();
+
+    @POST
+    Reservation make(Reservation reservation);
+
+    @GET
+    @Path("availability")
+    Collection<Car> availability(@RestQuery LocalDate startDate,
+                                 @RestQuery LocalDate endDate);
+}
