22546bf32213cacbd0d3c8a73ab200464bf50d99
xstefank <xstefank122@gmail.com>
2024-09-30 15:58:54 +0200

    8.5.1 Project Loom in Quarkus

--- chapter-08/8_5_1/loom-example/src/main/java/org/acme/GreetingResource.java
+++ chapter-08/8_5_1/loom-example/src/main/java/org/acme/GreetingResource.java
@@ -1,5 +1,7 @@
 package org.acme;
 
+import io.quarkus.logging.Log;
+import io.smallrye.common.annotation.RunOnVirtualThread;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -7,6 +9,14 @@ import jakarta.ws.rs.core.MediaType;
 
 @Path("/hello")
 public class GreetingResource {
+
+    // Invoked at :8080/hello/virtualThread
+    @GET
+    @Path("/virtualThread")
+    @RunOnVirtualThread
+    public void virtualThread() {
+        Log.info("Running on " + Thread.currentThread().getName());
+    }
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
