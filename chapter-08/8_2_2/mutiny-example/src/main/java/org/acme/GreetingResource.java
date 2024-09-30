7667e8d898ecec2777effc24cf82a288cec7e111
xstefank <xstefank122@gmail.com>
2024-09-30 15:06:07 +0200

    8.2.2 Mutiny API

--- chapter-08/8_2_2/mutiny-example/src/main/java/org/acme/GreetingResource.java
+++ chapter-08/8_2_2/mutiny-example/src/main/java/org/acme/GreetingResource.java
@@ -1,5 +1,6 @@
 package org.acme;
 
+import io.smallrye.mutiny.Multi;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -11,6 +12,22 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+
+        // The example in section 8.2.2
+        Multi.createFrom().items("a", "b", "c")
+            .onItem()
+                .transform(String::toUpperCase)
+            .onItem()
+                .invoke(s -> System.out.println("Intermediate stage " + s))
+            .onItem()
+                .transform(s -> s + " item")
+            .filter(s -> !s.startsWith("B"))
+            .onCompletion()
+                .invoke(() -> System.out.println("Stream completed"))
+            .subscribe()
+                .with(s -> System.out.println("Subscriber received " + s));
+
+
         return "Hello from Quarkus REST";
     }
 }
