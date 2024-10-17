5cf44b4b4253351746a372ed20bdb0448a884d7f
xstefank <xstefank122@gmail.com>
2024-10-17 08:10:22 +0200

    Start - 11.2.4 Pushing Car rental images to quay.io

---
+++ chapter-11/11_2_4/billing-service/src/main/java/org/acme/billing/data/ReservationInvoice.java
@@ -0,0 +1,9 @@
+package org.acme.billing.data;
+
+import org.acme.billing.model.Invoice;
+
+public class ReservationInvoice {
+
+    public Invoice.Reservation reservation;
+    public double price;
+}
