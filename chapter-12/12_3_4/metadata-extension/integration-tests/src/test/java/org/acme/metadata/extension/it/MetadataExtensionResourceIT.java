f02cc8d3b2a4498c31c5bb811e535904c8749dc6
xstefank <xstefank122@gmail.com>
2024-10-18 14:19:06 +0200

    Start - 12.3.4 Creating the recorder

---
+++ chapter-12/12_3_4/metadata-extension/integration-tests/src/test/java/org/acme/metadata/extension/it/MetadataExtensionResourceIT.java
@@ -0,0 +1,7 @@
+package org.acme.metadata.extension.it;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class MetadataExtensionResourceIT extends MetadataExtensionResourceTest {
+}
