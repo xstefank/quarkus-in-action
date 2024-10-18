0c57a2b16f90b6a75de4e114576781ddf6748fb1
xstefank <xstefank122@gmail.com>
2024-10-18 14:22:02 +0200

    Start - 12.3.5 Creating the processor

---
+++ chapter-12/12_3_5/inventory-service/src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO Car (licensePlateNumber, manufacturer, model) VALUES ('ABC123', 'Mazda', '6');
+INSERT INTO Car (licensePlateNumber, manufacturer, model) VALUES ('XYZ987', 'Ford', 'Mustang');
